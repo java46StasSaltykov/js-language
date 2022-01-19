c412ee9c700c9a4a95ac8afdc3315aa9a79aff11
StasSaltykov <stasyanych92@gmail.com>
2022-01-19 21:00:18 +0200

    homework#fix2

--- hw.js
+++ hw.js
@@ -12,9 +12,9 @@ console.log();
 function matrixTransp(matrix){
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < i; j++) {
-           const tmp = matrix[i][j];
+           const temp = matrix[i][j];
            matrix[i][j] = matrix[j][i];
-           matrix[j][i] = tmp;
+           matrix[j][i] = temp;
         };
      }
      console.log(matrix);
