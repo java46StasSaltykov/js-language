7106747c2682e9b080013394d2b636ab5d0ca291
StasSaltykov <stasyanych92@gmail.com>
2022-02-05 14:16:38 +0200

    cw & hw #17

--- app.js
+++ app.js
@@ -1,76 +1,104 @@
-const circle = {radius: 20, square: function() {
-        return 3.14 * (this.radius ** 2)
-}, perimeter: () => 2 * 3.14 * this.radius,
-toString: function(){
-        return `radious of this circle is ${this.radius}`
-}};
-console.log(`square=${circle.square()}, perimeter=${circle.perimeter()}`); 
-//circle.square will be converted to square(circle) where circle as argument value,
-// "this" is the hidden parameter in the function 
-// in any arrow function there is no "this"
-function square(circle) {
-        return 3.14 * (circle.radius ** 2);
+class Person {
+        #id; 
+        #name;
+        constructor(id, name) {
+                this.#id = id;
+                this.#name = name;
+        }
+        getId() {
+                return this.#id;
+        }
+        getName() {
+                return this.#name;
+        }
+        toString() {
+                return `id: ${this.#id}; name: ${this.#name};`;
+        }
+};
+
+const person = new Person(123, 'Moshe');
+// console.log(`person is ${person}`);
+
+class Employee extends Person {
+        #salary;
+        constructor (id, name, salary) {
+                super(id, name); // call the constructor of the class 'Person'
+                this.#salary = salary;
+        }
+        computeSalary() {
+                return this.#salary;
+        }
+        toString() {
+                return super.toString() + ` salary: ${this.computeSalary()};`; 
+        }
+};
+
+const person2 = new Employee(124, 'Sarah', 5000);
+// console.log(`person2 is ${person2}`);
+// console.log(typeof(person2)); // just object
+// console.log(person2.constructor.name); // only this way js allows getting constructor name
+
+class Child extends Person {
+        #kinderGarden;
+        constructor (id, name, kinderGarden) {
+                super(id, name);
+                this.#kinderGarden = kinderGarden;
+        }
+        getKinderGarden() {
+                return this.#kinderGarden;
+        }
+        toString() {
+                return super.toString() + ` kinderGarden: ${this.#kinderGarden};`;
+        }
+};
+
+const person3 = new Child(125, 'Yakov', 'Shalom');
+// console.log(`person3 is ${person3}`);
+
+class WageEmployee extends Employee {
+        #hours;
+        #wage;
+        constructor(id, name, salary, hours, wage){
+                super(id, name, salary);
+                this.#hours = hours;
+                this.#wage = wage;
+        }
+        computeSalary() {
+                return super.computeSalary() + this.#hours * this.#wage;
+        }
 }
-console.log(`circle: ${circle}`)
-const circle1 = {radius: 20,  perimeter: function() {
-                return 2 * 3.14 * this.radius}
-        ,
-        toString: function(){
-                return `radious of this circle is ${this.radius}`
-        }};
-        //circle1.square(); error because the method square is not defined inside object circle1
-        function Circle(radius) {
-                this.radius = radius;
-        }
-        Circle.prototype.square = function() {
-                return 3.14 * (this.radius ** 2);  
-        }
-        Circle.prototype.perimeter = function() {
-                return 2 * 3.14 * this.radius
-        }
-        Circle.prototype.toString = function() {
-                return `radius of this circle is ${this.radius}`
-        }
-        const circle10 = new Circle(10);
-        /*******************************************************HW #16 definition task 1 */
-//Write constructor Deferred
-        //         const d = new Deferred()
-// d.then(function(res){ console.log('1 ', res); return 'a'; });
 
-// d.then(function(res){ console.log('2 ', res); return 'b'; });
+const person4 = new WageEmployee(126, 'Asaf', 1000, 10, 100);
+// console.log(`person4 is ${person4}`);
 
-// d.then(function(res){ console.log('3 ', res); return 'c'; });
-// d.resolve('hello');
-//Output: 
-//1 hello
-//2 a
-//3 b
-/********************************************************* */
-/*******************************************HW #16 definition task2 */
-//write constructor MyArray
-//const myArray = new MyArray(10);
-//myArray.get(index) - result 10
-//write method get getting an index value and returning common value
-// (set in constructor)
-//myArray.set(index, value); 
-//write method set that sets a given value at a given index
-//myArray.setValue(value) - sets new value in all elements of myArray
-//Example:
-// const myArray = new MyArray(10);
-// console.log(myArray.get(100)) // displayed out 10
-// myArray.set(100, 500)//sets 500 at index 100
-// console.log(myArray.get(200)) //displayed out 10
-// console.log(myArray.get(100)) //displayed out 500
-// myArray.setValue(300);
-// console.log(myArray.get(100)) //displayed out 300
-// console.log(myArray.get(200)) //displayed out 300
+//////////////////////////////////   HOMEWORK  //////////////////////////////////////
 
-/***************************************************************************** */
-Array.prototype.filter = function(callbackPredicate) {
-        console.log('Tel-Ran copyright')
-        const res = []
-       this.forEach((n, i, a) => callbackPredicate(n, i, a) && res.push(n));
-       return res;
-}
-const ar = [1, 2, 4, 5, 100];
-ar.filter(n => n % 2 !== 0).forEach(n => console.log(n));
+const persons = [
+        new Child(100, 'Olya', 'Shalom'),
+        new Child(101, 'Serega', 'Boker'),
+        new Child(102, 'Kolya', 'Shalom'),
+        new Employee(103, 'Vasya', 1000),
+        new WageEmployee(104, 'Tolya', 1000, 10, 100)
+];
+
+function countOfPersonType(persons, type) {
+        let count = 0;
+        persons.forEach(element => element.constructor.name == type ? count++ : count);
+        return count;
+};
+console.log(countOfPersonType(persons, 'WageEmployee'));
+
+
+function computeSalaryBudget(persons) {
+        let count = persons.reduce((acc, cur) => 'computeSalary' in cur === true ? acc = acc + cur.computeSalary() : acc , 0);
+        return count;
+};
+console.log(computeSalaryBudget(persons));
+
+
+function countChildrenKindergarden(persons, kindergarden) {
+        let count = 0;
+        persons.forEach(element => 'getKinderGarden' in element === true && element.getKinderGarden() === kindergarden ? count++ : count);
+        return count;
+};
+console.log(countChildrenKindergarden(persons, 'Shalom'));
